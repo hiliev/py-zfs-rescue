5ccf6b38e5424f828a22a64dc2a52c859f3e6425
Konrad Eisele <eiselekd@gmail.com>
2018-04-14 12:09:01 +0200

    Use psize with minimum sector size check
    previouse patch changed to asize, however psize was right, however
    need to take into account the minimum sector size.

--- zfs/zio.py
+++ zfs/zio.py
@@ -77,7 +77,10 @@ class GenericDevice:
             lsize = bptr._embeded_lsize
             data = bptr._embeded_data
         else:
-            data = self._read_physical(offset, asize, debug_dump, debug_prefix)
+            rsize = psize
+            if psize < (1 << self._ashift):
+                rsize = 1 << self._ashift
+            data = self._read_physical(offset, rsize, debug_dump, debug_prefix)
         if bptr.compressed:
             if bptr.comp_alg in GenericDevice.CompType:
                 if self._verbose >= LOG_VERBOSE:
@@ -112,8 +115,9 @@ class GenericDevice:
 
 class MirrorDevice(GenericDevice):
 
-    def __init__(self, child_vdevs, proxy_addr, bad=None, dump_dir="/tmp"):
+    def __init__(self, child_vdevs, proxy_addr, ashift=9, bad=None, dump_dir="/tmp"):
         super().__init__(child_vdevs, proxy_addr, dump_dir=dump_dir)
+        self._ashift = ashift
         self._bad = bad
         if self._bad and len(self._bad) > len(self._devs):
             print("[-] Mirror created with more bad disks than copies!")
